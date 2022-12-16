04ad9f7e8c5c404298d5f0bb64681efb526f77c9
kasondra <sondra.fagan@gmail.com>
2022-12-16 13:37:56 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http.Headers;
+
+namespace _12._16test_kasondra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Coding is easy!");
+            Console.Write("So let's code today!");
+        }
+    }
+}
